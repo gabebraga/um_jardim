4ae82bebef15df1fa9edc141c1db38f4f4d92fd9
gabebraga <113937939+gabebraga@users.noreply.github.com>
2023-03-23 17:50:10 -0300

    ATTS e Header

---
+++ header.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                // openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+// sidebar
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
